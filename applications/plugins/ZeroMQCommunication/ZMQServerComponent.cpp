e99e39af4065dd6c854fe15f055a4207e13edccd
Bernardo Garcia <botibagl@gmail.com>
2018-02-06 00:59:09 -0500

    Replacing memcpy() by copy_n() to send data

--- applications/plugins/ZeroMQCommunication/ZMQServerComponent.cpp
+++ applications/plugins/ZeroMQCommunication/ZMQServerComponent.cpp
@@ -1,6 +1,9 @@
 #include <sofa/core/ObjectFactory.h>
 #include <zmq.hpp>
-#include <zmqpp/zmqpp.hpp>
+#include <zmq.h>
+#include <algorithm>
+#include <iterator>
+#include <zmqpp.hpp>
 #include <iostream>
 #include <string>
 #include "ZMQServerComponent.h"
@@ -22,17 +25,19 @@ namespace controller
 // zmq::context_t context(1);
 // zmq::socket_t socket(context, ZMQ_REP);
 
-/* variables para ZMQ Internal Client */
+/* variables para ZMQ Internal Client estas son */
 zmq::context_t context(1);
 zmq::socket_t socket(context, ZMQ_REQ);
 
-/*  initialize the 0MQ context
-zmqpp::context context;
+//  initialize the 0MQ context
+//zmqpp::context context;
 
 // generate a push socket
-zmqpp::socket_type type = zmqpp::socket_type::push;
-zmqpp::socket socket(context, type);
-*/
+//zmqpp::socket_type type = zmqpp::socket_type::push;
+//zmqpp::socket socket(context, type);
+
+
+
 
 ZMQServerComponent::ZMQServerComponent()
 // : myparam(initData(&myparam, 0.42, "myparam", "ZeroMq version plugin. "))
@@ -53,7 +58,7 @@ void ZMQServerComponent::setupConnection()
 void ZMQServerComponent::instrumentDataSend(instrumentData a)
 {
     
-    a.pos = sofa::defaulttype::Vec3d(1.0f, 1.0f, 1.0f);
+    a.pos = sofa::defaulttype::Vec3d(2.0f, 1.0f, 1.0f);
     a.quat = defaulttype::Quat(1.0f, 1.0f, 4.0f, 1.0f);
     a.btnState = 45;
     a.openInst = 1.0f;
@@ -61,34 +66,39 @@ void ZMQServerComponent::instrumentDataSend(instrumentData a)
 
     // ***************** btnState ***********************
     
-    //string s, test, result, d, s2, s3;
-    string btnState,  allData, posVector0;
+    string s, test, result;
+    string btnState, allData, posVector0, quatQuat2;
     posVector0 = to_string(a.pos[0]);
+    quatQuat2 = to_string(a.quat[2]);
     cout << posVector0;
 
     btnState = to_string(a.btnState);
-    // s2  = to_string(a.openInst);
+    s  = to_string(a.openInst);
     
-    // test = " is a number";
-    // result = s + test;
+    test = " is a number";
+    result = s + test;
     
-    /*
-    zmqpp::message message;
-    message << "Hello World!" << 42;
-    */
+    
+    //zmqpp::message message;
+    //message << "Hello World!" << 42;
+    
     // gettimeofday(&t_before, NULL);
-    zmq::message_t request(posVector0.size());
+    zmq::message_t request(quatQuat2.size() + 1);
     cout << "btnState is : " << a.btnState << endl;
     // cout << "btnState concatenate is: " << result << endl;
     // cout << "btnState concatenate with & is: " << &result << endl;
     
     // ***************** btnState ***********************
-    memcpy(request.data(), posVector0.c_str(), posVector0.size() + 1);
+    //memcpy(request.data(), posVector0.c_str(), posVector0.size() + 1);
+    //memcpy(request.data(), result.c_str(), result.size() + 1);
+    //std::copy_n(reinterpret_cast<char *>(request.data()), result.size(), result.c_str());
+    std::copy_n(quatQuat2.c_str(), quatQuat2.size() + 1, reinterpret_cast<char *>(request.data()));
+    //std::copy_n(result.c_str(), result.size() + 1, request.data());
     socket.send(request);
-    // socket.send(message);
+    //socket.send(message);
     // cout << "String btnState " << result << " sent to ZMQ Server" << endl;
 }
-
+/*
 void ZMQServerComponent::getResponseFromServer()
 {
     
@@ -103,7 +113,7 @@ void ZMQServerComponent::getResponseFromServer()
     printf("Time in microseconds: %ld microseconds\n",
            ((t_after.tv_sec - t_before.tv_sec) * 1000000L + t_before.tv_usec) - t_before.tv_usec);
 }
-
+*/
 void ZMQServerComponent::init()
 {
     std::cout << "ZeroMQCommunication::init()" << std::endl;
