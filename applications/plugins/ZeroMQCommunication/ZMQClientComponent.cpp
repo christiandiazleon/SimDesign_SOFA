7d5046c89ba5fbcc2e85cf148c77ce7e9ef8f616
Bernardo García <botibagl@gmail.com>
2018-05-29 18:21:29 -0500

    Starting to add msgPointsGroupV3 class to deformation points

--- applications/plugins/ZeroMQCommunication/ZMQClientComponent.cpp
+++ applications/plugins/ZeroMQCommunication/ZMQClientComponent.cpp
@@ -10,6 +10,7 @@
 #include <stdint.h>
 #include <random>
 #include "ZMQClientComponent.h"
+// #include "SurgicalProtocolV3.h"
 //#include "SofaTypeMessages.h"
 using namespace std;
 
@@ -188,7 +189,6 @@ void ZMQClientComponent::attachingDataToSend(attachingData b)
 
 }
 
-
 void ZMQClientComponent::init()
 {
     //fun();
@@ -212,9 +212,13 @@ void ZMQClientComponent::init()
 
     std::cout << "ZeroMQCommunication::init()" << std::endl;
     ZMQClientComponent z;
+    msgPointsGroupV3 msg;
+
+
 
     // Connecting to Nerwork Manager
     z.setupConnection();
+    msg.test();
 
 
     // float a = s->askDevice();
@@ -294,6 +298,195 @@ void ZMQClientComponent::draw()
 SOFA_DECL_CLASS(ZMQClientComponent)
 
 int ZMQClientComponentClass = sofa::core::RegisterObject("This component connect to ZMQ Socket.").add< ZMQClientComponent >();
+
+
+/*--------------------------------------msgPointsGroup----------------------------------  */
+msgPointsGroupV3::msgPointsGroupV3()
+{
+    NroPointsC = 0;
+    startPoint = 0;
+}
+void msgPointsGroupV3::test()
+{
+    std::cout << "Probando la instanciacion de clase" << std::endl;
+}
+/*
+void msgPointsGroupV3::FreeMemory()
+{
+	free(buffer);
+}
+
+bool msgPointsGroupV3::SetupSend()
+{
+	buffer = (char* )malloc(sizeof(char)*bufferSize);
+	char mystringIDC[10];
+	char mystringIDObj[10];
+    char mystringIDTMDef[10];
+    char mystringNroTotalPoints[10];
+    char mystringNroPoints[10];
+	char mystringPoint[10];
+    char mystringX[30];
+	char mystringY[30];
+	char mystringZ[30];
+    int NroPtoTemp;
+
+	sprintf(buffer, "%d", idTypeMessage);
+	sprintf(mystringIDC, "%d", FromIdMachine);
+	sprintf(mystringIDObj, "%d", IDBody);
+    sprintf(mystringIDTMDef, "%d", IDTypeMsgDef);
+    sprintf(mystringNroTotalPoints, "%d", NroTotalPoints);
+    sprintf(mystringNroPoints, "%d", NroPointsC);
+
+	strcat(buffer, " ");
+	strcat(mystringIDC, " ");
+	strcat(mystringIDObj, " ");
+    strcat(mystringIDTMDef, " ");
+    strcat(mystringNroPoints, " ");
+    strcat(mystringNroTotalPoints, " ");
+
+    strcat(buffer, mystringIDC);
+	strcat(buffer, mystringIDObj);
+    strcat(buffer, mystringIDTMDef);
+    strcat(buffer, mystringNroTotalPoints);
+    strcat(buffer, mystringNroPoints);
+
+
+    for (int i=0; i<NbPointsMsg; i++) {
+
+        if(NroPointsC>i)
+        {
+            NroPtoTemp = startPoint+i;
+            sprintf(mystringPoint, "%d", NroPtoTemp);
+            sprintf(mystringX, "%f", PointsC[i].getX());
+            sprintf(mystringY, "%f", PointsC[i].getY());
+            sprintf(mystringZ, "%f", PointsC[i].getZ());
+        }
+        else
+        {
+            NroPtoTemp = -1;
+            sprintf(mystringPoint, "%d", -1);
+            sprintf(mystringX, "%f", -1.0);
+            sprintf(mystringY, "%f", -1.0);
+            sprintf(mystringZ, "%f", -1.0);
+        }
+
+        strcat(mystringPoint, " ");
+        strcat(mystringX, " ");
+        strcat(mystringY, " ");
+        strcat(mystringZ, " ");
+
+        strcat(buffer, mystringPoint);
+        strcat(buffer, mystringX);
+        strcat(buffer, mystringY);
+        strcat(buffer, mystringZ);
+    }
+
+	return true;
+}
+
+bool msgPointsGroupV3::SetupReceive()
+{
+	float data[80];
+	string strTemp(buffer);
+
+	// construct a stream from the string
+	std::stringstream strstr(strTemp);
+
+	// use stream iterators to copy the stream to the vector as whitespace separated strings
+	std::istream_iterator<std::string> it(strstr);
+	std::istream_iterator<std::string> end;
+	std::vector<std::string> results(it, end);
+
+	if(results.size()==((NbPointsMsg*4)+6))
+	{
+		for(int i=0; i<results.size(); i++)
+		{
+			if(i == 0)
+			{
+				idTypeMessage = atoi(results[0].c_str());
+			}
+			else if(i == 1)
+			{
+				FromIdMachine = atoi(results[1].c_str());
+			}
+			else if(i == 2)
+			{
+				IDBody = atoi(results[2].c_str());
+			}
+			else if(i == 3)
+			{
+				IDTypeMsgDef = atoi(results[3].c_str());
+			}
+			else if(i == 4)
+			{
+				NroTotalPoints = atoi(results[4].c_str());
+			}
+			else if(i == 5)
+			{
+				NroPointsC = atoi(results[5].c_str());
+			}
+			else
+			{
+				data[i-6] = atof(results[i].c_str());
+			}
+		}
+
+		startPoint = (int)data[0];
+
+		for(unsigned int i=0;i<(NbPointsMsg*4);i+=4)
+		{
+			vNroPointC.push_back((int)data[i]);
+			PointNet pTemp(data[i+1], data[i+2], data[i+3]);
+			PointsC.push_back(pTemp);
+		}
+
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void msgPointsGroupV3::CompAmPointsByMsg()
+{
+
+}
+
+void msgPointsGroupV3::ResetData()
+{
+    FromIdMachine = 0;
+    idTypeMessage = 0;
+	IDBody = 0;
+    NroPointsC = 0;
+    PointsC.clear();
+    vNroPointC.clear();
+    startPoint = 0;
+}
+
+void msgPointsGroupV3::PrintMsg()
+{
+	/*std::cout << "IdTypeMsg: " << idTypeMessage << " IDMachine: " << FromIdMachine << " IDBody: " << IDBody << " IDTypeDefMsg: " << IDTypeMsgDef << "Nro Total: " << NroTotalPoints << " NroPoints " << NroPointsC << std::endl;
+
+    for (int i = 0; i<NroPointsC; i++) {
+
+        std::cout << "NroPoint: " << startPoint + i << " Point: " << PointsC[i].getX() << " " << PointsC[i].getY() << " " << PointsC[i].getZ() << std::endl;
+
+    }*/
+/*
+	std::cout << idTypeMessage << " " << FromIdMachine << " "  << IDBody << " " << IDTypeMsgDef << " " << NroTotalPoints << " " << NroPointsC << " ";
+
+    for (int i = 0; i<NroPointsC; i++) {
+
+        std::cout << startPoint + i << " " << PointsC[i].getX() << " " << PointsC[i].getY() << " " << PointsC[i].getZ() << " ";
+
+    }
+
+	std::cout << std::endl;
+}
+
+*/
+
 } // namespace controller
 
 } // namespace component
--- applications/plugins/ZeroMQCommunication/ZMQClientComponent.h
+++ applications/plugins/ZeroMQCommunication/ZMQClientComponent.h
@@ -59,11 +59,11 @@ class ZMQClientComponent : public sofa::core::behavior::BaseController
     // Data<std::string> d_address;
     // typedef sofa::defaulttype::Vec3d Vec3d;
     // typedef defaulttype::Quat Quat;0
-    
+
 
     ZMQClientComponent();
     virtual ~ZMQClientComponent(){};
-    
+
     /* Conect to ZMQ external Server  */
     void setupConnection();
     // void setupConnectionAttachingData();
@@ -101,6 +101,51 @@ class ZMQClientComponent : public sofa::core::behavior::BaseController
     string replyMessage;
     SerialDriver *s = new SerialDriver();
     string hapkitValue;
+};
+
+class msgPointsGroupV3
+{
+private:
+	int IDBody;
+  int IDTypeMsgDef; // 1 Start message - 2 Middle messages - 3 End message
+  int NroPointsC;
+  vector<PointNet> PointsC; // Vector que contiene todos los puntos de deformación del organo a ser enviados
+  vector<int> vNroPointC; // Vector que contiene el ID de cada punto de deformación del organo en la escena que sera enviado
+                            // Cada punto PointNet tiene un eje x,y,z  y un ID entonces debe construirse un mensaje asi:
+                            // ID1_X1_Y1_Z1-ID2_X2_Y2_Z2
+
+  int startPoint;
+  int NroTotalPoints;
+	int NbPointsMsg; // Número de puntos que se envían por mensaje
+
+public:
+	msgPointsGroupV3();
+
+  void test();
+
+	void setIDBody(int value){IDBody = value;}
+  void setNroPointsC(int value){NroPointsC = value;}
+  void setStartPoint(int value){startPoint = value;}
+  void setPointsC(vector<PointNet> value){PointsC = value;}
+  void setIDTypeMsgDef(int value){IDTypeMsgDef = value;}
+  void setNroTotalPoints(int value){NroTotalPoints = value;}
+	void setNbPointsMsg(int value){NbPointsMsg = value;}
+
+	int getIDBody(){return IDBody;}
+  int getNroPointsC(){return NroPointsC;}
+  int getStartPoint(){return startPoint;}
+  int getIDTypeMsgDef(){return IDTypeMsgDef;}
+  int getNroTotalPoints(){return NroTotalPoints;}
+	int getNbPointsMsg(){return NbPointsMsg;}
+	vector<PointNet> getPointsC(){return PointsC;}
+  vector<int> getVecNroPointsC(){return vNroPointC;}
+
+	bool SetupSend();
+	bool SetupReceive();
+  void CompAmPointsByMsg();
+  void ResetData();
+  void PrintMsg();
+	void FreeMemory();
 };
 
 
